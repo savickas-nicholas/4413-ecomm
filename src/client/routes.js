6c29dba771f24bb41f5673a5609ae25816ceb875
Jkal24 <jkalirai2499@gmail.com>
2023-12-03 17:20:10 -0500

    Reverting newest change in routes

--- src/client/routes.js
+++ src/client/routes.js
@@ -65,7 +65,7 @@ export default [
         element: <Checkout />,
       },
       {
-        path: "/orders/:id",
+        path: "/orders/:orderId",
         element: <Summary />,
       },
     ],
