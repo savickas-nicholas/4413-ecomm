1cd947b210df344573fb77512e1ecd9c67c9308b
savickas-nicholas <77470784+savickas-nicholas@users.noreply.github.com>
2023-12-06 16:09:09 -0500

    cart UI update

--- src/client/components/Cart/Cart.js
+++ src/client/components/Cart/Cart.js
@@ -50,52 +50,59 @@ export default function Cart() {
   return (
     <div className='cartContainer'>
       { items.length > 0 ? 
-        <div className='cart flex-column'>
-          <div className='cartList'>
-            { items.map(el => {
-              let vehicle = el.item;
-              return (
-                <div className='cartElem flex-row'>
-                  <img src={getImageByPath(vehicle.imgPath)} width='300' />
-                  <div className='flex-column-spread fill'>
-                    <div>{`${vehicle.brand} ${vehicle.model}`}</div>
-                    <div className='actionBar flex-row'>
-                      <div>
-                        Quantity: <input type='number' value={el.quantity} 
-                          min='1' max={vehicle.quantity} step='1'
-                          onChange={(e) => handleUpdateCart(vehicle, e.target.value)} />
-                      </div>
-                      <div className='false-link' 
-                        onClick={(e) => handleRemoveFromCart(el.item._id)}>Remove</div>
+        <div className='flex-content-sidebar'>
+          <div className='cart flex-column content-el'>
+            <div className='cartList'>
+              { items.map(el => {
+                let vehicle = el.item;
+                return (
+                  <div className='cartElem padding-10  border rounded flex-row'>
+                    <div className='flex-centered'>
+                      <img src={getImageByPath(vehicle.imgPath)} width='300' />
                     </div>
-                  </div>
-                  <div className='flex-centered'>
-                    { vehicle && vehicle.discount && vehicle.discount > 0  ?
-                      <h4><s>${vehicle.price}</s></h4>
-                      :
-                      <h4><strong>${vehicle.price}</strong></h4>
-                    }
-                    { vehicle && vehicle.discount && vehicle.discount > 0  ?
-                      <h4 className='promo'><strong>PROMO: ${vehicle.price - (vehicle.price * vehicle.discount)}</strong></h4>
-                      :
-                      <div></div>
-                    }
-                  </div>
-                </div>  
-              );
-            })}
+                    <div className='flex-column-spread fill'>
+                      <div>{`${vehicle.brand} ${vehicle.model}`}</div>
+                      <div className='actionBar flex-row'>
+                        <div>
+                          Quantity: <input type='number' value={el.quantity} 
+                            min='1' max={vehicle.quantity} step='1'
+                            onChange={(e) => handleUpdateCart(vehicle, e.target.value)} />
+                        </div>
+                        <div className='false-link' 
+                          onClick={(e) => handleRemoveFromCart(el.item._id)}>Remove</div>
+                      </div>
+                    </div>
+                    <div className='flex-centered'>
+                      { vehicle && vehicle.discount && vehicle.discount > 0  ?
+                        <h4><s>${vehicle.price}</s></h4>
+                        :
+                        <h4><strong>${vehicle.price}</strong></h4>
+                      }
+                      { vehicle && vehicle.discount && vehicle.discount > 0  ?
+                        <h4 className='promo'><strong>PROMO: ${vehicle.price - (vehicle.price * vehicle.discount)}</strong></h4>
+                        :
+                        <div></div>
+                      }
+                    </div>
+                  </div>  
+                );
+              })}
+            </div>
+            <div className='flex-centered'>
+              <button className='btn btn-secondary' onClick={() => handleClearCart()}>Clear Cart</button>
+            </div>      
           </div>
-          <div className='flex-centered'>
-            <button className='btn btn-secondary' onClick={() => handleClearCart()}>Clear Cart</button>
-          </div>
-          <div className='flex-centered'>
-            <a href='/checkout' className='btn btn-primary' style={{color: 'white'}}>Proceed to Checkout</a>
-          </div>
-          <div className='flex-centered'>
-            <span>{`Subtotal (${cartDetails.count} items): ${cartDetails.subtotal}`}</span>
-          </div>
+          <div className='flex-column sidebar-el'>
+              <div className='flex-centered' style={{marginTop: '30px'}}>
+                <span>{`Subtotal (${cartDetails.count} items): `}<b>${cartDetails.subtotal}</b></span>
+              </div>
+              <div className='flex-centered'>
+                <a href='/checkout' className='btn btn-primary' style={{color: 'white'}}>Proceed to Checkout</a>
+              </div>
+            </div>
         </div>
-      : <div>Your cart is empty!</div>}
+      : <div>Your cart is empty!</div>
+      }
     </div>
   );
 }
